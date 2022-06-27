7a423aec52986c6e11bdf85d827a26b48d8034f2
Aneel Atwal <aneelatwal@Dhars-iMac.local>
2022-06-26 18:29:32 -0700

    question 1 unchanged

--- a3.h
+++ a3.h
@@ -1,49 +1,26 @@
+#include <iostream>
+using std::cout;
+using std::endl;
 
 
+//Question 1
+//PARAM: arr is array to print the Cartesian product of, n is size of arr
+void cartesianProduct(int arr[], int n)
+ {
+    int i = 0;
+    while (i < n) {
+            int j = 0;
+            while (j < n) {
+                cout << "{" << arr[i] << "," << arr[j] << "}";
+                j++;
+                cout << " ";
+            }
 
-// Desc:  Implementation of an int sequence with push/pop in a LIFO order
-class Stack {
-
-    private:
-
-        // Desc:  arr = static array of capacity STACKCAP
-        //        size = the number of elements presently in the stack
-        //  Inv:  Follows the A2 Spec, namely that the stack elements 
-        //        are in order within A[0..size-1], where the top of
-        //        the stack is A[0].
-        static const unsigned STACKCAP = 8;
-        int arr[STACKCAP];
-        unsigned size;
-
-    public:
-
-        // Desc:  Object constructor
-        // Post:  Initialize the size of the stack to 0
-        Stack();
+            cout << endl;
+            i++;
+    }
+ }
 
 
-        // Desc:  Insert element x to the top of the stack.
-        //  Pre:  size < STACKCAP
-        // Post:  Insert element x at index 0 in the array and shift all elements in the array index + 1
-        void push(int x);
-
-
-        // Desc:  Remove and return element at the top of the stack.
-        //  Pre:  size > 0
-        // Post:  Remove and return element from the top of the stack and shift all elements in the array index - 1
-        int pop();
-
-
-        // Desc:  Return the topmost element of the stack.
-        //  Pre:  size > 0
-        // Post:  Return the element from the stack at index 0
-        int peek() const;
-
-
-        // Desc:  Check if the stack is empty.
-        // Post:  Returns true/false whether the stack is empty
-        bool isEmpty() const;
-
-};
-
-
+ //Question 2
+ 
